401c7372bef7df28999c3efbf8e4801eefe4932b
David Andersson <lejonmanen@gmail.com>
2020-05-19 10:26:26 +0200

    State changes, CSS transition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    // State variables
+    const [reluctantGrow, setReluctantGrow] = useState(false);
+
+    // "Computed properties"
+    let reluctantCssClass = 'reluctant-grow';
+    if( reluctantGrow ) {
+        reluctantCssClass += ' active';
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                header
+                Animation demo
             </header>
             <main>
-                main
+                <section className="grow">
+                    <button onClick={() => setReluctantGrow(!reluctantGrow)}> I will grow </button>
+                    <div className={reluctantCssClass}> Click button to grow me </div>
+                </section>
             </main>
         </div>
     );
